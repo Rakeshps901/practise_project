fa4ab55eed5780e1178920b365e8c1ca53892d2e
Rakeshps901 <adithyaraj012003@gmail.com>
2023-08-02 13:28:36 +0530

    initial commit

--- lib/registration.dart
+++ lib/registration.dart
@@ -1,76 +1,34 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:practise_project/registrationpage.dart';
 
-class Registration extends StatelessWidget {
+void main()
+{
+  runApp(MaterialApp(home: Register(),));
+}
+class Register extends StatefulWidget {
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 1),()
+    {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context)=>Registration2()));
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Registration Page"),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.person),
-                    hintText: "Full Name",
-                    labelText: "FullName"),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.phone),
-                    hintText: "Phone Number",
-                    labelText: "PhoneNumber"),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.people),
-                    hintText: "UserName",
-                    helperText: "user name must be an email",
-                    labelText: "UserName"),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 20, right: 20, bottom: 50),
-              child: TextField(
-                obscureText: true,
-                obscuringCharacter: '*',
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.visibility_off_sharp),
-                    hintText: "Password",
-                    helperText: "password must contain 6 characters",
-                    labelText: "Password"),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 20, right: 20, bottom: 50),
-              child: TextField(
-                obscureText: true,
-                obscuringCharacter: '*',
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.visibility_off_sharp),
-                    hintText: "Conform Password",
-                    labelText: "Conform Password"),
-              ),
-            ),
-          ],
-        ),
+      body: Center(
+        child: Image.asset("assets/icons/rk.jpg"),
       ),
     );
   }
-
 }
